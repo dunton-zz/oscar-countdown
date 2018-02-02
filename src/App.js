7e160f4653e6fb38269363dd6006e12d06569f0d
Ryan Dunton <ryan.dunton@area23hc.com>
2018-02-02 16:17:42 -0500

    made curtains auto open

--- src/App.js
+++ src/App.js
@@ -2,12 +2,33 @@ import React, { Component } from 'react';
 import SoundBar from './components/sound'
 import Clock from './components/clock';
 class App extends Component {
- 
+
+  constructor() {
+    super();
+
+    this.state = {
+      appClass: 'theatre'
+    }
+  }
+   
+  componentDidMount() {
+
+    //open curtains after 2 seconds
+
+    this.animator = () => {
+      setTimeout(() => {
+        this.setState({ appClass: 'theatre open' })
+      }, 2000);
+    }
+
+    this.animator();
+    
+  }
 
   render() {
     return (
       <div className="background">
-        <div className="theatre">
+        <div className={this.state.appClass}>
           <em className="curtain-left"></em>
           <em className="curtain-right"></em>
           <p className="header-text">COUNTDOWN TO THE OSCARS!!!</p>
